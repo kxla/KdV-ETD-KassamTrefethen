eb0ce53098cf97bd9d81ac0139b12cc04ad055d7
mollymerritt <merritt.mry@gmail.com>
2017-05-18 15:53:04 -0400

    Update KT-ETDRK4.m

--- KT-ETDRK4.m
+++ KT-ETDRK4.m
@@ -31,9 +31,9 @@ f2 = h*real(mean( (2+LR+exp(LR).*(-2+LR))./LR.^3 ,2));
 f3 = h*real(mean( (-4-3*LR-LR.^2+exp(LR).*(4-LR))./LR.^3 ,2));
 
 % Main time-stepping loop:
-tdata = zeros(nplots+1,1);
-uu = u; tt = 0;
 tmax = 150; nmax = round(tmax/h); nplt = floor((tmax/100)/h);
+uu = zeros(nmax,N);
+tdata = h:h:tmax;
 g = 1i*k*(3/4)*alpha; % nonlinear operator
 for n = 1:nmax
   t = n*h;
@@ -45,20 +45,15 @@ for n = 1:nmax
   c = E2.*a + Q.*(2*Nb-Nu_hat);
   Nc = g.*fft(real(ifft(c)).^2);
   u_hat = E.*u_hat + Nu_hat.*f1 + 2*(Na+Nb).*f2 + Nc.*f3;
-  tdata(n+1,1) = t;
   if mod(n,nplt)==0
     u = real(ifft(u_hat));
-    uu = [uu,u]; tt = [tt,t];
+    uu(n,:) = u;
   end
 end
 
 % Plot results:
-%surf(tt,x,uu), shading interp, lighting phong, axis tight
-%view([-90 90]), colormap(autumn); set(gca,'zlim',[-5 50])
-%light('color',[1 1 0],'position',[-1,2,2])
-%material([0.30 0.60 0.60 40.00 1.00]);
 figure
 waterfall(x,tdata,real(uu)), view(0,70),
 xlim([-Left,Left]);
-ylim([0,t_max]);
+ylim([0,tmax]);
 grid off
